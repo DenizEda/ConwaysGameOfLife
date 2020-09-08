81361dedcd994cc2d112f20eefbc907a245663b4
Eda Deniz <edeniz9191@gmail.com>
2020-09-07 23:58:35 -0400

    update GameOfLife.js

--- GameOfLife.js
+++ GameOfLife.js
@@ -5,73 +5,93 @@ class GameOfLife {
     this.board = this.makeBoard();
   }
 
-  getCell(row, col) {
-    if (this.board[row][col]) {
-      return this.board[row][col];
+  makeBoard() {
+    const myBoard = new Array(this.height).fill(0);
+    return myBoard.map(() => {
+      const cell = new Array(this.width).fill(0);
+      return cell;
+    });
+  }
+
+  cellExists(row, col) {
+    if (row >= 0 && row < this.height && col >= 0 && col < this.width) {
+      return true;
     } else {
-      return "dead";
+      return false;
     }
   }
 
-  setCell(val, row, col) {
-    if (this.board[row][col]) {
-      this.board[row][col] = val;
+  getCell(row, col) {
+    if (this.cellExists(row, col)) {
       return this.board[row][col];
     } else {
-      return "dead";
+      return 0;
+    }
+  }
+
+  setCell(value, row, col) {
+    if (this.cellExists(row, col)) {
+      this.board[row][col] = value;
+    } else {
+      return 0;
     }
   }
 
   toggleCell(row, col) {
-    if (this.board[row][col]) {
+    if (this.cellExists(row, col)) {
       if (this.board[row][col] === 1) {
-        this.board[row][col] = 2;
-      } else {
-        this.board[row][col] = 1;
+        this.setCell(0, row, col);
+      } else if (this.board[row][col] === 0) {
+        this.setCell(1, row, col);
       }
-      return this.board[row][col];
     } else {
-      return "dead";
+      return;
     }
-  }
-
-  makeBoard() {
-    let i = 0;
-    let j = 0;
-    let row = [];
-    let newBoard = [];
-    while (i < this.width) {
-      row.push(1);
-      i++;
-    }
-
-    while (j < this.height) {
-      newBoard.push(row);
-      j++;
-    }
-    this.board = newBoard;
-    return this.board;
   }
 
   livingNeighbors(row, col) {
-    // TODO: Return the count of living neighbors.
+    return (
+      this.getCell(row - 1, col - 1) +
+      this.getCell(row - 1, col) +
+      this.getCell(row - 1, col + 1) +
+      this.getCell(row, col - 1) +
+      this.getCell(row, col + 1) +
+      this.getCell(row + 1, col - 1) +
+      this.getCell(row + 1, col) +
+      this.getCell(row + 1, col + 1)
+    );
   }
 
-  /**
-   * Given the present board, apply the rules to generate a new board
-   */
+  conwayRule(cell, livingNeighbors) {
+    const isAlive = cell === 1;
+    if (isAlive) {
+      if (livingNeighbors === 2 || livingNeighbors === 3) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else if (livingNeighbors === 3) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  forEachCell(iterator) {
+    for (let row = 0; row < this.height; row++) {
+      for (let col = 0; col < this.width; col++) {
+        iterator(row, col);
+      }
+    }
+  }
 
   tick() {
     const newBoard = this.makeBoard();
-    // TODO: Here is where you want to loop through all the cells
-    // on the existing board and determine, based on it's neighbors,
-    // whether the cell should be dead or alive in the new board
-    // (the next iteration of the game)
-    //
-    // You need to:
-    // 1. Count alive neighbors for all cells
-    // 2. Set the next state of all cells in newBoard,
-    // based on their current alive neighbors
+    this.forEachCell((row, col) => {
+      const livingNeighbors = this.livingNeighbors(row, col);
+      const nextCell = this.conwayRule(this.getCell(row, col), livingNeighbors);
+      newBoard[row][col] = nextCell;
+    });
     this.board = newBoard;
   }
 }
